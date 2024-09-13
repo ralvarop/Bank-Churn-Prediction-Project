fc71b85073db079d30fff394c2d499e19d38177e
Raúl AP <150734564+ralvarop@users.noreply.github.com>
2024-09-13 10:37:46 -0500

    Update test.py

--- src/test.py
+++ src/test.py
@@ -19,8 +19,8 @@ def eval_model(filename):
     model = pickle.load(open(package, 'rb'))
     print('Modelo importado correctamente')
     # Predecimos sobre el set de datos de validación 
-    X_test = df.drop(['DEFAULT'],axis=1)
-    y_test = df[['DEFAULT']]
+    X_test = df.drop(['Exited'],axis=1)
+    y_test = df[['Exited']]
     y_pred_test=model.predict(X_test)
     # Generamos métricas de diagnóstico
     cm_test = confusion_matrix(y_test,y_pred_test)
@@ -36,7 +36,7 @@ def eval_model(filename):
 
 # Validación desde el inicio
 def main():
-    df = eval_model('credit_val.csv')
+    df = eval_model('test.csv')
     print('Finalizó la validación del Modelo')
 
 
